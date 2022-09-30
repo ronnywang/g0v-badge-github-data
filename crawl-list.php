7742b333511927bfe49155ccffdec37345d11fad
Ronny Wang <ronnywang@gmail.com>
2022-09-30 16:56:09 +0800

    add crawl-list script

---
+++ crawl-list.php
@@ -0,0 +1,18 @@
+<?php
+
+$columns = [
+    'name', 'description', 'created_at', 'updated_at', 'pushed_at', 'size', 'stargazers_count', 'watchers_count', 'language', 'forks_count', 'forks', 'open_issues',
+];
+
+$output = fopen('php://output', 'w');
+for ($p = 1; true; $p ++) {
+    $url = "https://api.github.com/orgs/g0v/repos?page=" . $p;
+    $cmd = sprintf("curl %s", escapeshellarg($url));
+    $records = json_decode(`$cmd`);
+    if (!count($records)) {
+        break;
+    }
+    foreach ($records as $record) {
+        fputcsv($output, array_map(function($c) use ($record) { return $record->{$c}; }, $columns));
+    }
+}
